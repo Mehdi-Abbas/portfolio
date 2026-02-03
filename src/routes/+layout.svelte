c131ea2b5e0f932c7be744923696f576a7cc651a
Mehdi Abbas <mehdi.abbas@ids-astra.com>
2026-02-03 19:43:42 +0500

    fix tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,15 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZP77ZZMX6X"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-ZP77ZZMX6X');
+	</script>
+</svelte:head>
 {@render children()}
